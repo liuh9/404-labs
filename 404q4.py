b2ed91640b939222d7e9e92e5706765ea3bedefd
Hao Liu <liu9@ualberta.ca>
2021-09-06 02:18:38 -0600

    Update 404q4.py

--- 404q4.py
+++ 404q4.py
@@ -8,3 +8,7 @@ r = requests.get('http://www.google.com/')
 print(r.url)
 print(r)
 print(r.text)
+print("")
+
+code = requests.get('https://raw.githubusercontent.com/liuh9/404-labs/main/404q4.py')
+print(code.text)
